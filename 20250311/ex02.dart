f348a945542913edb3bfd898bd173c6239775581
inatagan <22266171+inatagan@users.noreply.github.com>
2025-03-18 16:19:19 -0300

    fixes

--- 20250311/ex02.dart
+++ 20250311/ex02.dart
@@ -1,4 +1,4 @@
-class Laptop {
+class House {
   String? id;
   String? nome;
   String? preco;
@@ -9,8 +9,8 @@ var lista = [];
 void main() {
   for (var i = 0; i < 3; i++) {
     // O nome da classe eh o construtor
-    Laptop(this.id = i, this.nome= "nome ${i}", this.preco= i+16);
-    print(Laptop);
-    lista.insert(Laptop);
+    House(this.id = i, this.nome= "nome ${i}", this.preco= i+16);
+    print(House);
+    lista.insert(House);
   }
 }
