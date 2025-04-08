93291c5e0d7b703b8abdde5f83d0cd04c99d9015
inatagan <22266171+inatagan@users.noreply.github.com>
2025-04-08 18:16:15 -0300

    Login app hotfix

--- 20250325/ex01/lib/pages/home_page.dart
+++ 20250325/ex01/lib/pages/home_page.dart
@@ -2,17 +2,29 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   // late final User user;
+  late final String email;
 
-  HomePage() {
-    //TO DO
+  HomePage({required String email}) {
+    this.email = email;
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(children: [Center(child: Text('TEST'))]),
+      appBar: AppBar(title: const Text('Home Page')),
+      body: Column(
+        children: [
+          Text(email),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Go back!"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- 20250325/ex01/lib/pages/login.dart
+++ 20250325/ex01/lib/pages/login.dart
@@ -1,54 +1,111 @@
 import 'package:app_login/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-// class Login extends StatefulWidget {
-//   const Login({super.key});
-//
-//   @override
-//   FormLogin createState() {
-//     // TODO: implement createState
-//     return FormLogin();
-//   }
-// }
+class Login extends StatefulWidget {
+  const Login({super.key});
 
-class Login extends StatelessWidget {
-  // final _formkey = GlobalKey<FormState>();
+  @override
+  State<Login> createState() {
+    return _Login();
+  }
+}
+
+class _Login extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
-      // appBar: AppBar(title: Center(child: Text('Login to continue..'))),
-      // key: _formkey,
-      body: Column(
-        children: [
-          Padding(padding: EdgeInsets.all(12.0)),
+      appBar: AppBar(title: Text('Login')),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8,
+                  vertical: 16,
+                ),
+                child: TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Email",
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8,
+                  vertical: 16,
+                ),
+                child: TextFormField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Password",
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8,
+                  vertical: 16.0,
+                ),
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        // Navigate the user to the Home page
 
-          TextFormField(
-            obscureText: true,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Insira a sua senha!',
-            ),
-            // The validator receives the text that the user has entered.
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
+                        if (emailController.text == "test@email.com" &&
+                            passwordController.text == "1234") {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder:
+                                  (context) =>
+                                      HomePage(email: emailController.text),
+                            ),
+                          );
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Invalid Credentials'),
+                            ),
+                          );
+                        }
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Please fill input')),
+                        );
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                ),
+              ),
+            ],
           ),
-          //ADD Botoa aqui
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage()),
-              );
-            },
-            child: const Text('Login'),
-          ),
-        ],
+        ),
       ),
     );
   }
